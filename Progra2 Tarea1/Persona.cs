a56902bb3c8789e9c819ab6546d9ee394e801d71
LeonardoLopez11 <leonardolopeztolentino@gmail.com>
2024-03-26 13:25:56 -0500

    creacion del Constructor  y Metodo

---
+++ Progra2 Tarea1/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra2_Tarea1
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+    }
+}
